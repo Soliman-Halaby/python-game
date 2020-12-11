db38a0c733d79fd1d752b3aa4697b71375b1d5b3
Soliman Al Halaby <60599592+Soliman-Halaby@users.noreply.github.com>
2020-12-11 11:25:24 +0100

    :star: First Upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# def Menu():
+#   print('1 - Nouvelle partie')
+#   print('2 - Informations')
+#   print('3 - Quitter')
+    
+#     ChoixMenu = int(input())
+#   if ChoixMenu == 1:
+#     newGame()
+
+#   elif ChoixMenu == 2:
+#     info()
+
+#   elif ChoixMenu == 3:
+#     leave()
+
+print('Coucou')
